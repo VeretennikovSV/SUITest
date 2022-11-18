4147c5df78e6d66771c3ab1ef78f27b0aaeb8ad8
Sergei <kopgamerk@gmail.com>
2022-11-18 09:35:37 +0300

    Initial Commit

---
+++ SUITest/SUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITestApp.swift
+//  SUITest
+//
+//  Created by Сергей Веретенников on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
